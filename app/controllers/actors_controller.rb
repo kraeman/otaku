fdf63e555d7940e542be9a905b3e08ce1dcb92b4
Krae <kraewind@gmail.com>
2021-03-31 18:33:52 -0400

    wrote actor controller crud actions and strong params

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,40 @@
 class ActorsController < ApplicationController
+    def new
+        @actor = Actor.new
+    end
+    
+    def create
+        @actor = Actor.create(actor_params)
+        if @actor.id
+          session[:actor_id] = @actor.id
+          redirect_to @actor
+        else
+          render :new
+        end
+    end
+    
+    def show
+        @actor = Actor.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        @actor.update(actor_params)
+        if @actor.valid?
+            redirect_to @actor
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+    end
+    
+    private
+    
+    def actor_params
+        params.require(:actor).permit(:name, :age)
+    end
 end
