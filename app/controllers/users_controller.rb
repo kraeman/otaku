37249e30fd459e0efaed2772633581d4cd9dbfbb
Krae <kraewind@gmail.com>
2021-03-31 18:24:59 -0400

    Wrote user controller new create show and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.create(user_params)
+        if @user.id
+          session[:user_id] = @user.id
+          redirect_to @user
+        else
+          render :new
+        end
+    end
+    
+    def show
+        @user = current_user
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :DOB, :name, :admin)
+    end
 end
