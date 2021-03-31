964e134ce5ee59e87aa21c1fd3bcaedd9129d2d2
Krae <kraewind@gmail.com>
2021-03-31 18:22:07 -0400

    Added UN PW and email columns to user table and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_202352) do
+ActiveRecord::Schema.define(version: 2021_03_31_221955) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2021_03_31_202352) do
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
   end
 
   create_table "viewings", force: :cascade do |t|
