ddebb0bd8b6c31f3e6d3de0936e474291a04a93a
Krae <kraewind@gmail.com>
2021-03-31 18:35:50 -0400

    wrote character controller crud actions and strong params

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,40 @@
 class CharactersController < ApplicationController
+    def new
+        @character = Character.new
+    end
+    
+    def create
+        @character = Character.create(character_params)
+        if @character.id
+          session[:character_id] = @character.id
+          redirect_to @character
+        else
+          render :new
+        end
+    end
+    
+    def show
+        @character = Character.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        @character.update(character_params)
+        if @character.valid?
+            redirect_to @character
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+    end
+    
+    private
+    
+    def character_params
+        params.require(:character).permit(:name, :bio, :show_id)
+    end
 end
