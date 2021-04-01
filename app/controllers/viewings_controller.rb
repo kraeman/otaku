2c4ee787e9c88e124d7421080e079b99e124985d
krae <kraewind@gmail.com>
2021-04-01 10:38:54 -0400

    wrote viewing controller actions

--- app/controllers/viewings_controller.rb
+++ app/controllers/viewings_controller.rb
@@ -1,2 +1,40 @@
 class ViewingsController < ApplicationController
+    def new
+        @viewing = Viewing.new
+    end
+    
+    def create
+        @viewing = Viewing.create(viewing_params)
+        if @viewing.id
+          session[:viewing_id] = @viewing.id
+          redirect_to @viewing
+        else
+          render :new
+        end
+    end
+    
+    def viewing
+        @viewing = Viewing.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        @viewing.update(viewing_params)
+        if @viewing.valid?
+            redirect_to @viewing
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+    end
+    
+    private
+    
+    def viewing_params
+        params.require(:viewing).permit(:time, :place, :show_id, :user_id)
+    end
 end
