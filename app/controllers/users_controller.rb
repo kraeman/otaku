2d17f213b521b0beca034f858811ec876fbca2b3
krae <kraewind@gmail.com>
2021-04-01 11:07:38 -0400

    changed DOB to dob in strong params of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
     private
     
     def user_params
-        params.require(:user).permit(:username, :email, :password, :DOB, :name, :admin)
+        params.require(:user).permit(:username, :email, :password, :dob, :name, :admin)
     end
 end
