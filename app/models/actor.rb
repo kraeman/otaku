14d0572c3a5c697aaff7909a5a99732d4c5ae6d8
krae <kraewind@gmail.com>
2021-04-01 15:05:15 -0400

    Validations added to actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,7 @@
 class Actor < ApplicationRecord
     has_many :shows, through: :actors_shows
     has_many :characters
+
+    validates :name, presence: true
+    #add validation for dob being in the past and being a date 
 end
