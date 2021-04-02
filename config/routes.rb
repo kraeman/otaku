8a27d98f35780e8788f1b92b2c8b8920e4b97368
krae <kraewind@gmail.com>
2021-04-02 12:50:18 -0400

    fixed routing to POST /login

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :users
   resources :comments
   resources :shows
+  get '/login', to: "sessions#new", as: "signin"
+  post '/login', to: "sessions#create"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
