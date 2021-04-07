863c5307b7065a1552c0bb8db3f6a5acdc243598
krae <kraewind@gmail.com>
2021-04-06 23:26:51 -0400

    Search now has default scope of being in alphabetical order relative to other objects of same class

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,6 +5,6 @@ class Actor < ApplicationRecord
 
     validates :name, presence: true
     #add validation for dob not being in the future
-
+    default_scope { order(name: :asc) }
     scope :search_by_name, -> (name) {where("name LIKE ?", "#{name}%")}
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -5,6 +5,6 @@ class Character < ApplicationRecord
   validates :name,  presence: true
   validates :bio, presence: true
 
-
+  default_scope { order(name: :asc) }
   scope :search_by_name, -> (name) {where("name LIKE ?", "#{name}%")}
 end
