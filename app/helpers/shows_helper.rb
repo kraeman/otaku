be5dfaa5ecabccd7be5f67e8637b3f4973d7e584
krae <kraewind@gmail.com>
2021-04-13 17:59:59 -0400

    fixed display shows on index helper method

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,11 +1,15 @@
 module ShowsHelper
     def display_shows_conditionally(shows)
+        anime = ""
+        i = 1
          if shows.any? 
              shows.each do |s| 
-                (link_to s.title, show_path(s))
-             end 
-             else 
+                anime << (link_to s.title, show_path(s)) + content_tag(:br)
+             end
+             anime.html_safe
+        else 
                 "No anime yet! Add one or check back later!"
-             end 
+        end
+
     end
 end
