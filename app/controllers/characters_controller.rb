c8047c69fe6360c215985adfd04498ac506fb984
krae <kraewind@gmail.com>
2021-04-13 12:58:57 -0400

    added helper methods to characters create

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -18,9 +18,9 @@ class CharactersController < ApplicationController
     end
     
     def create
-        if !character_params[:show_attributes]
-            if character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != ""
-                if params[:show_id] != character_params[:show_id]
+        if no_show_attributes_in_character_params?
+            if character_params_filled_out?
+                if hacked?
                     redirect_to new_show_character_path(Character.find(params[:show_id]))
                 else
                     @character = Character.create(character_params)
@@ -31,8 +31,8 @@ class CharactersController < ApplicationController
                 render :new
             end
         else             
-                if character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != "" && (character_params[:show_id] != "" || (character_params[:show_attributes][:title] != "" && character_params[:show_attributes][:rating] != "" && character_params[:show_attributes][:air_time] != ""))
-                    if character_params[:show_id] != ""
+                if character_params_filled_out_with_show_attributes?
+                    if show_id_in_character_params
                         @character = Character.create(name: character_params[:name], bio: character_params[:bio], show_id: character_params[:show_id], actor_id: character_params[:actor_id])
                         redirect_to @character
                     else
@@ -83,5 +83,26 @@ class CharactersController < ApplicationController
     def character_params
         params.require(:character).permit(:name, :bio, :show_id, :actor_id, :avatar, show_attributes: [:title, :air_time])
     end
+
+    def no_show_attributes_in_character_params?
+        !character_params[:show_attributes]
+    end
+
+    def character_params_filled_out?
+        character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != ""
+    end
+
+    def hacked?
+        params[:show_id] != character_params[:show_id]
+    end
+
+    def character_params_filled_out_with_show_attributes?
+        character_params_filled_out? && (character_params[:show_id] != "" || (character_params[:show_attributes][:title] != "" && character_params[:show_attributes][:rating] != "" && character_params[:show_attributes][:air_time] != ""))
+    end
+
+    def show_id_in_character_params
+        character_params[:show_id] != ""
+    end
+
 
 end
--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,11 +2,11 @@ module SearchesHelper
     def display_search_results(results)
          results.each do |r| 
              if r.class == Show 
-                content_tag(li: link_to r.title, show_path(r))
+                content_tag(li: (link_to r.title, show_path(r)))
              elsif r.class == Character 
-                content_tag(li: link_to r.name, character_path(r))
+                content_tag(li: (link_to r.name, character_path(r)))
              else 
-                content_tag(li: link_to r.name, actor_path(r))
+                content_tag(li: (link_to r.name, actor_path(r)))
              end 
          end 
     end
