94982c4335ef7fa10bd5e9ebb23b867c19d0c3cf
krae <kraewind@gmail.com>
2021-04-12 20:38:54 -0400

    Added DOB validator to actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,7 +4,15 @@ class Actor < ApplicationRecord
     has_many :characters
 
     validates :name, presence: true
-    #add validation for dob not being in the future
+    validate :date_in_future
     default_scope { order(name: :asc) }
     scope :search_by_name, -> (name) {where("name LIKE ?", "#{name}%")}
+private
+  
+    def date_in_future
+        if dob >= Time.now
+            errors.add(:dob, " can not be in the future!")
+        end
+    end
 end
+  
