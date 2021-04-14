deedaa0a7abac76d8908d693e1572283b878028b
krae <kraewind@gmail.com>
2021-04-14 17:27:52 -0400

    cleaned logic for character creation

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -22,7 +22,18 @@ class CharactersController < ApplicationController
     end
     
     def create
-        context_of_show_and_filled_out_correctly_and_security(character_params)       
+       @character = helpers.context_of_show_and_filled_out_correctly_and_security(character_params)
+        if helpers.context_of_show?(character_params) && helpers.filled_out_form_correctly_in_context_of_show?(character_params)
+                if helpers.hacked?(character_params)
+                    redirect_to new_show_character_path(helpers.find_in_context_of_show(character_params))
+                else
+                    redirect_to show_character_path(helpers.find_in_context_of_show_but_not_hacked(character_params), @character)
+                end
+        elsif @character.valid?
+            redirect_to @character
+        else
+            render :new
+        end       
     end
     
     def show
@@ -59,53 +70,6 @@ class CharactersController < ApplicationController
     
     def character_params
         params.require(:character).permit(:name, :bio, :actor_id, :show_id, :avatar, show_attributes: [:title, :air_time, :show_id, :id])
-    end
-
-    def context_of_show?
-        !character_params[:show_id]
-    end
-
-    def filled_out_form_correctly_in_context_of_show?
-        character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != ""    end
-
-    def hacked?
-        character_params[:show_attributes][:show_id] != character_params[:show_attributes][:id]
-    end
-
-    def filled_out_form_correctly?
-        character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != "" && (character_params[:show_id] != "" || (character_params[:show_attributes][:title] != "" && character_params[:show_attributes][:rating] != "" && character_params[:show_attributes][:air_time] != ""))    end
-
-    def selected_show_from_dropdown?
-        character_params[:show_id] != ""
-    end
-
-    def context_of_show_and_filled_out_correctly_and_security(character_params)
-        if context_of_show?
-            if filled_out_form_correctly_in_context_of_show?
-                if hacked?
-                    redirect_to new_show_character_path(Show.find(character_params[:show_attributes][:id]))
-                else
-                    @character = Character.create(name: character_params[:name], bio: character_params[:bio], actor_id: character_params[:actor_id], show_id: character_params[:show_attributes][:id])
-                    redirect_to show_character_path(Show.find(character_params[:show_attributes][:id]), @character)
-                end
-            else
-                @character = Character.create(name: character_params[:name], bio: character_params[:bio], actor_id: character_params[:actor_id], show_id: character_params[:show_attributes][:id])
-                render :new
-            end
-        else             
-                if filled_out_form_correctly?
-                    if selected_show_from_dropdown?
-                        @character = Character.create(name: character_params[:name], bio: character_params[:bio], show_id: character_params[:show_id], actor_id: character_params[:actor_id])
-                        redirect_to @character
-                    else
-                        @character = Character.create(character_params)
-                        redirect_to @character
-                    end
-                else
-                 @character = Character.create(character_params)
-                 render :new
-                end  
-        end
     end
 
 
--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -26,4 +26,43 @@ module CharactersHelper
                 "No characters yet! Add one or check back later!"
         end
     end
+
+    def context_of_show?(character_params)
+        !character_params[:show_id]
+    end
+
+    def filled_out_form_correctly_in_context_of_show?(character_params)
+        character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != ""    end
+
+    def hacked?(character_params)
+        character_params[:show_attributes][:show_id] != character_params[:show_attributes][:id]
+    end
+
+    def filled_out_form_correctly?(character_params)
+        character_params[:name] != "" && character_params[:bio] != "" && character_params[:actor_id] != "" && (character_params[:show_id] != "" || (character_params[:show_attributes][:title] != "" && character_params[:show_attributes][:rating] != "" && character_params[:show_attributes][:air_time] != ""))    end
+
+    def selected_show_from_dropdown?(character_params)
+        character_params[:show_id] != ""
+    end
+
+    def context_of_show_and_filled_out_correctly_and_security(character_params)
+        if context_of_show?(character_params) && filled_out_form_correctly_in_context_of_show?(character_params)
+                return Character.create(name: character_params[:name], bio: character_params[:bio], actor_id: character_params[:actor_id], show_id: character_params[:show_attributes][:id])
+        else             
+                    if selected_show_from_dropdown?(character_params)
+                        return Character.create(name: character_params[:name], bio: character_params[:bio], show_id: character_params[:show_id], actor_id: character_params[:actor_id])
+                    else
+                        return Character.create(character_params)
+                    end
+        end
+    end
+
+    def find_in_context_of_show(character_params)
+        Show.find(character_params[:show_attributes][:id])
+    end
+
+    def find_in_context_of_show_but_not_hacked(character_params)
+        Show.find(character_params[:show_attributes][:id])
+    end
+
 end
